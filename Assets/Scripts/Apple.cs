f9a87b29a5e5b674878dc3ae8d55b2cb36ebc5a2
Michael Marmer <mm1755@scarletmail.rutgers.edu>
2015-06-11 20:00:37 -0400

    Complete Apple Picker

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Apple : MonoBehaviour {
+
+	public static float bottomY = -20f;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (transform.position.y < bottomY) {
+			Destroy (this.gameObject);
+
+			ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+			apScript.AppleDestroyed();
+		}
+	}
+}
---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScore : MonoBehaviour {
+
+	static public int score = 1000;
+	
+
+	void Awake() {
+
+		if (PlayerPrefs.HasKey ("ApplePickerHighScore")) {
+			score = PlayerPrefs.GetInt("ApplePickerHighScore");
+		}
+
+		PlayerPrefs.SetInt ("ApplePickerHighScore", score);
+	}
+
+	void Update () {
+	
+		GUIText gt = this.GetComponent<GUIText> ();
+		gt.text = "High Score: " + score;
+
+		if (score > PlayerPrefs.GetInt ("ApplePickerHighScore")) {
+			PlayerPrefs.SetInt("ApplePickerHighScore", score);
+		}
+	}
+}
